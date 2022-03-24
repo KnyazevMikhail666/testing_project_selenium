af5bcf32f623771fd29fa249988ec97e8cc94d21
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-03-24 17:39:25 +0300

    create files base_page.py and main_page.py base_page.py: add class BasePage main_page.py: add class MainPage(BasePage) test_main_page.py: refactor fun. test_guest_can_go_to_login_page()

--- conftest.py
+++ conftest.py
@@ -5,17 +5,17 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose languages: es or fr")
+                     help="Choose languages:")
+
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     language = request.config.getoption("language")
-    browser, options = None, Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': language if language else None})
-    if language == "es" or language == 'fr':
-        print("\nstarting browser in es languages for test.." if language == 'es'
-              else "\nstarting browser in fr languages for test..")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    if language:
+        print(f"\nstarting browser in {language} languages for test..")
         browser = webdriver.Chrome(options=options)
     else:
         raise pytest.UsageError("--languages should be es or fr")
