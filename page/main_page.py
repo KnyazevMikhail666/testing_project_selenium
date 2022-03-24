e7ee1ea304e81f33a218c76ff12a22772665db90
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-03-24 17:57:29 +0300

    base_page.py:class BasePage: add function checked the element 'is_element_present()' main_page.py:class MainPage:add function 'should_be_login_link()'in your is used previous function

--- page/main_page.py
+++ page/main_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+
 from .base_page import BasePage
 
 
@@ -7,3 +8,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), \
+            "Login link is not presented"
