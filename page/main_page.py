72213d17d645a8636a57392d6f611009edb841cf
KnyazevMikhail666 <81702840+KnyazevMikhail666@users.noreply.github.com>
2022-03-26 14:31:53 +0300

    create files: locators.py and login_page.py locators.py: add class : MainPageLocators, LoginPageLocators : they contain variables CSS selectors and urls login_page.py: add class: LoginPage(BasePage): they contains validation tests: the correctness of the link login form availability register form availability

--- page/main_page.py
+++ page/main_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-
+from .locators import MainPageLocators
 from .base_page import BasePage
 
 
@@ -9,6 +9,7 @@ class MainPage(BasePage):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
 
+
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), \
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), \
             "Login link is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,5 @@
+from .page.login_page import LoginPage
 from .page.main_page import MainPage
-
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -7,3 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
